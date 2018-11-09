e63a5fb7950410419f812999215a79023036e55c
nyuqiu <tomeczek110@gmail.com>
2018-11-09 16:00:10 +0000

    Calculator

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,38 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Modul 6. Wprowadzenie do testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        System.out.println("Test - pierwszy test jednostkowy:");
+
+        Calculator calculator = new Calculator();
+
+        int resultOfSubstracting = calculator.substract(10, 7);
+
+        if(resultOfSubstracting==(10-7)){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        int resultOfAdding = calculator.addition(10, 7);
+
+        if(resultOfAdding==(10+7)){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,13 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+
+    public int substract(int minuend, int subtrahend){
+        return minuend-subtrahend;
+    }
+
+    public int addition(int component1, int component2){
+        return component1+component2;
+    }
+
+}
