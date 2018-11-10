16327b4d93c664bc6cec6381f0a0cc5713086625
nyuqiu <tomeczek110@gmail.com>
2018-11-10 09:45:55 +0000

    Calculator

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,8 +1,35 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
+
 public class TestingMain {
     public static void main(String[] args){
 
+        SimpleUser simpleUser = new SimpleUser("theForumUser", "Johnny Q");
+
+        String result = simpleUser.getUsername();
+        if (result.equals("theForumUser")){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        // tu umieść kolejny test ->
+        System.out.println("Test - pierwszy test jednostkowy:");
+
+
+        Calculator calculator = new Calculator();
+
+        int addResult = calculator.addition(7, 3);
+        int subResult = calculator.substract(7, 3);
+
+        if((addResult == 10) && (subResult == 4)){
+            System.out.println("Calculator test OK");
+        } else {
+            System.out.println("Calculator has error!");
+        }
 
     }
 }
