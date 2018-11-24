0e684d933c7cf271b26108757f65e29d91d3823d
nyuqiu <tomeczek110@gmail.com>
2018-11-24 16:43:41 +0000

    Stream7.5

--- kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
@@ -4,16 +4,17 @@ import java.util.stream.IntStream;
 
 public class ArrayOperations {
     public static double getAverage(int[] numbers){
-        if(numbers.length==0) {
-            return 0;
-        }
-            IntStream showElements = IntStream.range(0, numbers.length)
-                    .map(n -> numbers[n]);
-            System.out.println(showElements);
+//        if(numbers.length==0) {
+//            return 0;
+//        }
+//            IntStream showElements = IntStream.range(0, numbers.length)
+//                    .map(n -> numbers[n]);
+//            System.out.println(showElements);
 
             double getAverage = IntStream.range(0, numbers.length)
                     .map(n -> numbers[n])
-                    .average().getAsDouble();
+                    .peek(n -> System.out.println(n))
+                    .average().orElse(0);
             return getAverage;
 
     }
