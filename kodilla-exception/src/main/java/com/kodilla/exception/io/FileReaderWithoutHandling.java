76ca262856fbf71a74919a46dd9902cb8efd7220
nyuqiu <tomeczek110@gmail.com>
2018-11-25 22:37:30 +0000

    Exceptions 8.3

--- kodilla-exception/src/main/java/com/kodilla/exception/io/FileReaderWithoutHandling.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/io/FileReaderWithoutHandling.java
@@ -6,22 +6,22 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.stream.Stream;
 
-public class FileReader {
+public class FileReaderWithoutHandling {
 
-    public void readFile(){
+    public void readFile() throws FileReaderException{
         ClassLoader classLoader = getClass().getClassLoader();
         File file = new File(classLoader.getResource("file/names").getFile());
 
-        try (Stream<String> fileLines = Files.lines(Paths.get(file.getPath()))) {
-
+        try(Stream<String> fileLines = Files.lines(Paths.get(file.getPath()))){
             fileLines.forEach(System.out::println);
-
-        } catch(IOException e){
-            System.out.println("Huston, we have problem"+e);
+        } catch(IOException e) {
+            throw new FileReaderException();
         } finally {
             System.out.println("I am gonna be here... always!");
         }
 
-        System.out.println(file.getPath());
+
+
     }
 }
+
--- kodilla-exception/src/main/java/com/kodilla/exception/main/ExceptionModuleRunner.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/main/ExceptionModuleRunner.java
@@ -1,11 +1,26 @@
 package com.kodilla.exception.main;
 
-import com.kodilla.exception.io.FileReader;
+import com.kodilla.exception.io.FileReaderException;
+import com.kodilla.exception.io.FileReaderWithoutHandling;
+import com.kodilla.exception.test.ExceptionHandling;
+import com.kodilla.exception.test.SecondChallenge;
 
 public class ExceptionModuleRunner {
     public static void main(String args[]){
 
-        FileReader fileReader = new FileReader();
-        fileReader.readFile();
+        FileReaderWithoutHandling fileReader = new FileReaderWithoutHandling();
+        SecondChallenge secondChallenge = new SecondChallenge();
+
+        try{
+            fileReader.readFile();
+        } catch(FileReaderException e){
+            System.out.println("Problem while reading a file!");
+        }
+
+        try{
+            secondChallenge.probablyIWillThrowException(2,1.5 );
+        } catch (ExceptionHandling e){
+            System.out.println("It should be like that");
+        }
     }
 }
