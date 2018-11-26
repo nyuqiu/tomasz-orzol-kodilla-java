3913cf504ed13f462a63f5e4e8f5ceb83afd7a0a
nyuqiu <tomeczek110@gmail.com>
2018-11-26 16:18:42 +0000

    Exceptions 8.5 Not ready

--- kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
@@ -3,19 +3,12 @@ package com.kodilla.stream.array;
 import java.util.stream.IntStream;
 
 public class ArrayOperations {
-    public static double getAverage(int[] numbers){
-//        if(numbers.length==0) {
-//            return 0;
-//        }
-//            IntStream showElements = IntStream.range(0, numbers.length)
-//                    .map(n -> numbers[n]);
-//            System.out.println(showElements);
-
-            double getAverage = IntStream.range(0, numbers.length)
-                    .map(n -> numbers[n])
-                    .peek(n -> System.out.println(n))
-                    .average().orElse(0);
-            return getAverage;
+    public static double getAverage(int[] numbers) {
+        double getAverage = IntStream.range(0, numbers.length)
+                .map(n -> numbers[n])
+                .peek(n -> System.out.println(n))
+                .average().orElse(0);
+        return getAverage;
 
     }
 }
