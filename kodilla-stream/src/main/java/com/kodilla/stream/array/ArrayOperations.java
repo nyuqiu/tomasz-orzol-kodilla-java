fe9ed026481aefa6fbe06996651b377efa68fb23
nyuqiu <tomeczek110@gmail.com>
2018-12-03 16:22:36 +0000

    Ready

--- kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
@@ -4,7 +4,6 @@ import java.util.stream.IntStream;
 
 public class ArrayOperations {
     public static double getAverage(int[] numbers) {
-
         double getAverage = IntStream.range(0, numbers.length)
                 .map(n -> numbers[n])
                 .peek(n -> System.out.println(n))
