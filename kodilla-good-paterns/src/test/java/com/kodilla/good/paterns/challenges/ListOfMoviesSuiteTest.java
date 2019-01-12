daa0f8eabc8397ce262de6dbb838274885a0b50f
nyuqiu <tomeczek110@gmail.com>
2019-01-12 13:27:06 +0000

    GoodPaterns

---
+++ kodilla-good-paterns/src/test/java/com/kodilla/good/paterns/challenges/ListOfMoviesSuiteTest.java
@@ -0,0 +1,16 @@
+package com.kodilla.good.paterns.challanges;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ListOfMoviesSuiteTest {
+    @Test
+    public void testShowListOfMovies(){
+        //Given
+
+        //When
+
+        //Then
+        Assert.assertEquals("Żelazny Człowiek!Iron Man!Mściciele!Avengers!Błyskawica!Flash");
+    }
+}
