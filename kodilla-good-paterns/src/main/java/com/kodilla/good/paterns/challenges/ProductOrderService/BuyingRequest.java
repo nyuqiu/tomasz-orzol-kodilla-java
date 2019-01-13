9b2b17d04c15608cbb3827bed8265744647fe361
nyuqiu <tomeczek110@gmail.com>
2019-01-13 00:13:40 +0000

    GoodPaterns13.2

--- kodilla-good-paterns/src/main/java/com/kodilla/good/paterns/challenges/ProductOrderService/BuyingRequest.java
+++ kodilla-good-paterns/src/main/java/com/kodilla/good/paterns/challenges/ProductOrderService/BuyingRequest.java
@@ -14,7 +14,7 @@ class BuyingRequest {
 
     public boolean createOrder(User user, String product, String address) {
         if(product!=null && address!=null) {
-            System.out.println("Creating order for "+ user.getName()+user.getSurname()+" with product "+product+ ", for address "+address);
+            System.out.println("Creating order for "+ user.getName()+" "+user.getSurname()+" with product "+product+ ", for address "+address);
             return true;
         } else {
             System.out.println("Product not avaiable/Address not correct");
