244378aa9770a24a574cd3eec20c3403607d8520
nyuqiu <tomeczek110@gmail.com>
2019-01-28 05:27:17 +0000

    Patterns15.2

---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/prototype/library/LibraryTestSuite.java
@@ -0,0 +1,50 @@
+package com.kodilla.patterns.prototype.library;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.time.LocalDate;
+import java.util.stream.IntStream;
+
+public class LibraryTestSuite {
+
+    @Test
+    public void testGetBooks(){
+        //Given
+        Library library = new Library("First library");
+        IntStream.iterate(1, n-> n+1)
+                .limit(10)
+                .forEach(n -> library.getBooks().add(new Book("Book nr "+n, "Author nr "+n, LocalDate.of(n+1990, n%12, n))));
+        Book book = new Book("Test", "Test autor", LocalDate.of(1,1,1));
+        library.getBooks().add(book);
+        Library clonedLibrary = null;
+        try {
+            clonedLibrary = library.shallowCopy();
+            clonedLibrary.setName("Shallow copy library");
+        } catch (CloneNotSupportedException e){
+            System.out.println(e);
+        }
+
+        Library deepClonedLibrary = null;
+        try {
+            deepClonedLibrary = library.deepCopy();
+            deepClonedLibrary.setName("Deep copy library");
+            } catch (CloneNotSupportedException e){
+            System.out.println(e);
+        }
+
+        //When
+        library.getBooks().remove(book);
+        //Then
+        System.out.println(library);
+        System.out.println(clonedLibrary);
+        System.out.println(deepClonedLibrary);
+        Assert.assertEquals(10, library.getBooks().size());
+        Assert.assertEquals(10, clonedLibrary.getBooks().size());
+        Assert.assertEquals(11, deepClonedLibrary.getBooks().size());
+        Assert.assertEquals(clonedLibrary.getBooks(), library.getBooks());
+        Assert.assertNotEquals(deepClonedLibrary.getBooks(), library.getBooks());
+        }
+    }
+
+
