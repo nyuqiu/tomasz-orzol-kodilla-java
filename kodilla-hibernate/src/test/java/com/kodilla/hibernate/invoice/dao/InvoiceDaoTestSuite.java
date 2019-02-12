e6b101a408f693a4aae417cc24ced74c5d88a3ed
lukasz.barc <lukasz.barc@cosmose.co>
2019-02-12 21:57:10 +0100

    teraz dziala :)

--- kodilla-hibernate/src/test/java/com/kodilla/hibernate/invoice/dao/InvoiceDaoTestSuite.java
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/invoice/dao/InvoiceDaoTestSuite.java
@@ -19,48 +19,23 @@ public class InvoiceDaoTestSuite {
     private InvoiceDao invoiceDao;
 
     @Autowired
-    private ProductDao productDao;
-
-    @Autowired
     private ItemDao itemDao;
 
     @Test
-    public void testInvoiceDaoSave(){
+    public void testInvoiceDaoSave() {
         //Given
-        Item item = new Item(BigDecimal.valueOf(2), 4);
-        Item item1 = new Item(BigDecimal.valueOf(3), 5);
-        Item item2 = new Item(BigDecimal.valueOf(4),6);
-        Product product = new Product("bread");
-        Product product1 = new Product("lasagne");
-        Invoice invoice = new Invoice("2345");
         Invoice invoice1 = new Invoice("6789");
-        item.setProduct(product);
-        item1.setProduct(product);
-        item2.setProduct(product1);
-        item.setInvoice(invoice);
-        item1.setInvoice(invoice);
-        item2.setInvoice(invoice1);
+
+        Item item1 = new Item(BigDecimal.valueOf(3), 5, new Product("lasagne"));
+        Item item2 = new Item(BigDecimal.valueOf(4), 6, new Product("bread"));
+        Item item3 = new Item(BigDecimal.valueOf(2), 4, new Product("spaghetti"));
+
+
+        invoice1.addItem(item1);
+        invoice1.addItem(item2);
+        invoice1.addItem(item3);
 
         //When
-        productDao.save(product);
-        int productId = product.getId();
-        productDao.save(product1);
-        int product1Id = product1.getId();
-        invoiceDao.save(invoice);
-        int invoiceId = invoice.getId();
         invoiceDao.save(invoice1);
-        int invoice1Id = invoice1.getId();
-
-        //Then
-        Assert.assertNotEquals(0,invoiceId);
-        Assert.assertNotEquals(0,invoice1Id);
-        Assert.assertNotEquals(0,productId);
-        Assert.assertNotEquals(0,product1Id);
-
-        //CleanUp
-        productDao.delete(productId);
-        productDao.delete(product1Id);
-        invoiceDao.delete(invoiceId);
-        invoiceDao.delete(invoice1Id);
-        }
+    }
 }
