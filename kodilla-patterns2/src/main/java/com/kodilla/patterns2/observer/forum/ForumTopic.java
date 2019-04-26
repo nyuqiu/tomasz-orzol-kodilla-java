af15a081e7a39eb5e2a43f579827649a139f2406
nyuqiu <tomeczek110@gmail.com>
2019-04-26 18:22:04 +0100

    Added Observer pattern

---
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/observer/forum/ForumTopic.java
@@ -0,0 +1,56 @@
+package com.kodilla.patterns2.observer.forum;
+
+import javafx.beans.InvalidationListener;
+import javafx.beans.Observable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ForumTopic implements Observable {
+    private final List<Observer> observers;
+    private final List<String> messages;
+    private final String name;
+
+    public ForumTopic(String name) {
+        observers = new ArrayList<>();
+        messages = new ArrayList<>();
+        this.name = name;
+    }
+
+    public void addPost(String post) {
+        messages.add(post);
+        notifyObservers();
+    }
+
+    public void registerObserver(Observer observer) {
+        observers.add(observer);
+    }
+
+    public void notifyObservers() {
+        for (Observer observer : observers) {
+            observer.update(this);
+        }
+    }
+
+    public void removeObserver(Observer observer) {
+        observers.remove(observer);
+    }
+
+    public List<String> getMessages() {
+        return messages;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void addListener(InvalidationListener listener) {
+
+    }
+
+    @Override
+    public void removeListener(InvalidationListener listener) {
+
+    }
+}
